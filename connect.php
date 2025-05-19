d72d353a910c228c0312cfb0180f8e583c1cc133
Izeno <78334105+VoxDroid@users.noreply.github.com>
2025-05-19 12:50:47 +0900

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,8 +5,13 @@ $servername = "localhost";
 $username = "root";
 $password = $env['DB_PASSWORD'];
 $db_name = "sc_district";
+
+// Production Connection (Uncomment Later)
 $conn = mysqli_connect($servername, $username, $password, $db_name);
 if (!$conn) {
 	die("Connection Error " . mysqli_connect_error());
 } else {
 }
+
+// Test Connection (Uncomment To use)
+// $conn = null;
