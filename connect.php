4c6ea8d83f5f262d1abea6ac61a966873b7707ad
Izeno <78334105+VoxDroid@users.noreply.github.com>
2025-05-19 12:07:56 +0900

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php
 //Sequence server, username, password, database name
+$env = parse_ini_file(".env");
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = $env['DB_PASSWORD'];
 $db_name = "sc_district";
 $conn = mysqli_connect($servername, $username, $password, $db_name);
 if (!$conn) {
