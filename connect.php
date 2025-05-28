aa63466f8c4d713e56eab2863f6d37b9dcea8648
Izeno <78334105+VoxDroid@users.noreply.github.com>
2025-05-28 16:44:36 +0900

    remove hardcoded database credentials

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 //Sequence server, username, password, database name
 $env = parse_ini_file(".env");
-$servername = "localhost";
-$username = "root";
+$servername = $env['DB_SERVERNAME'];
+$username = $env['DB_USERNAME'];
 $password = $env['DB_PASSWORD'];
-$db_name = "sc_district";
+$db_name = $env['DB_NAME'];
 
 // Production Connection (Uncomment Later)
 $conn = mysqli_connect($servername, $username, $password, $db_name);
